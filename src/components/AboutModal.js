0cd99cec7c86f2b8a267299055191dd3c661c573
keonp <keonlmp@gmail.com>
2022-10-10 18:36:00 -0400

    about modal

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -3,8 +3,13 @@ function AboutModal() {
 
     return (
         <div className="aboutPage">
+            <h2>Sudoku solver</h2>
+            <p>Are you struggling with a tough Sudoku puzzle? Fret not! With this Sudoku Solver, you can find the answer to any Sudoku puzzle, no matter how difficult!</p>
+
             <h2>What do I need to do?</h2>
-            <p>Simply put,</p>
+            <p>With the current puzzle you are working on in hand, input the numbers into each square until the puzzle on screen matches the base puzzle you have. Then press the 'solve puzzle' button and watch as the solver completes the puzzle in an instant!</p>
+            <p>When you're done with the completed puzzle, you can select 'restart' to clear the board and solve a new puzzle.</p>
+            <p><strong>You may also use your keyboard instead of the number buttons on screen!</strong></p>
         </div>
     )
 }
