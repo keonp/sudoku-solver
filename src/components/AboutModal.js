cd677fd10f8f8e1f9ab08732f4d1660432369e4a
keonp <keonlmp@gmail.com>
2022-10-10 23:42:22 -0400

    final touches

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -17,12 +17,12 @@ function AboutModal({setAboutToggle}) {
                 <p>Are you struggling with a tough Sudoku puzzle? Fret not! With this Sudoku Solver, you can find the answer to any Sudoku puzzle no matter how difficult!</p>
 
                 <h2>What do you need to do?</h2>
-                <p>With the current puzzle you are working on in hand, input the numbers into each square until the puzzle on screen matches the base puzzle you have. Then press the 'solve puzzle' button and watch as the solver completes the puzzle in an instant!</p>
-                <p>When you're done with the completed puzzle, you can select 'restart' to clear the board and solve a new puzzle.</p>
+                <p>With the current puzzle you are working on in hand, input the numbers into each square until the puzzle on screen matches the base puzzle you have. Then press the <span className="highlight">solve puzzle</span> button and watch as the solver completes the puzzle in an instant!</p>
+                <p>When you're done with the completed puzzle, you can select the <span className="highlight">restart</span> button to clear the board and solve a new puzzle.</p>
                 <p><strong>You may also use your keyboard instead of the number buttons on screen!</strong></p>
 
                 <h2>How does it work?</h2>
-                <p>The program utilizes a recursive backtracking algorithm/brute force method to solve the puzzle. It attempts to input a number in an empty cell, starting from the cell in the first row and column, while complying with the rules of sudoku. It will continue to do so until it completes the puzzle or gets to a cell where no numbers can be used. In that instance, it will backtrack to the previous cell and attempt a different number, where it will then continue forward or backtrack again.</p>
+                <p>The program utilizes a recursive backtracking algorithm/brute force method to solve the puzzle. It attempts to input a number in an empty cell, starting from the cell in the first row and column, while complying with the rules of sudoku. It will continue to do so until it completes the puzzle or gets to a cell where no numbers can be used. In that instance, it will backtrack to the previous cell and attempt a different number, where it will then continue forward or backtrack again until the puzzle is solved.</p>
             </div>
         </div>
     )
